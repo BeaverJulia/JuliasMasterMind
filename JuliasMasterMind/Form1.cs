47802d7479f7737b929b42f627d286507a0fefbf
Julia <jgula@edu.cdv.pl>
2018-11-22 17:46:11 +0100

    My Version Of master mind

--- JuliasMasterMind/Form1.cs
+++ JuliasMasterMind/Form1.cs
@@ -10,11 +10,126 @@ using System.Windows.Forms;
 
 namespace JuliasMasterMind
 {
+
     public partial class Form1 : Form
     {
+        Color[] arrayOfcolors;
+        List<Button> ColorsFirstRow;
+        List<Button>  ColorsSecondRow;
+        List<Button> ColorsThirdRow;
+        List<Button> ColorsFourthRow;
+        List<Button> ColorsFifthRow;
+        List<Button> ColorssixthRow;
+        List<Button> ColorsseventhRow;
+        int[] indexofColors;
+        
         public Form1()
         {
             InitializeComponent();
+            
+            arrayOfcolors = new Color[] { Color.White, Color.Aqua, Color.Coral, Color.DarkGreen, Color.DeepPink, Color.Yellow };
+            Random random = new Random();
+
+            int[]
+            List<Color> colors = new List<Color>(5);
+                for (int i = 0; i < 5; i++)
+            {
+                
+                int randomcolor = random.Next(1, 5);
+
+                colors[i] = arrayOfcolors[randomcolor];
+                
+            }
+
+
+
+
+
+            
+            List<Control> list = new List<Control>();
+
+            foreach (var control in this.Controls)
+            {
+                if (control.GetType() == typeof(Button))
+                {
+                    Button temp = (Button)control;
+                    switch (temp.Tag)
+                    {
+                            case "first":
+                            {
+                                ColorsFirstRow.Add(temp);
+                                break;
+                            }
+                        case "second":
+                            {
+                                ColorsSecondRow.Add(temp);
+                                break;
+                            }
+                        case "third":
+                            {
+                                ColorsThirdRow.Add(temp);
+                                break;
+                            }
+                        case "fourth":
+                            {
+                                ColorsFourthRow.Add(temp);
+                                break;
+                            }
+                        case "fifth":
+                            {
+                                ColorsThirdRow.Add(temp);
+                                break;
+                            }
+                        case "sixth":
+                            {
+                                ColorssixthRow.Add(temp);
+                                break;
+                            }
+                        case "seventh":
+                            {
+                                ColorsseventhRow.Add(temp);
+                                break;
+                            }
+                    }
+                }
+            }
+
+          
+        }
+
+        public void ChangeColor(object sender, EventArgs e)
+        {
+            Color actual;
+            Button clickbutton = (Button)sender;
+            indexofColors = new int[5] { 0, 0, 0, 0, 0 };
+            for (int i = 0; i < 6; i++)
+            {
+                if (clickbutton.BackColor == arrayOfcolors[i])
+                {
+                    if (i == 5)
+                    {
+                        clickbutton.BackColor = arrayOfcolors[0];
+                    }
+                    else
+                    {
+                        clickbutton.BackColor = arrayOfcolors[i + 1];
+                    }
+                    break;
+                }
+            }
+
+        }
+        public class Checker
+        {
+            
+        }
+    
+      
+
+        
+        private void Check(object sender, EventArgs e)
+        {
+
         }
     }
 }
